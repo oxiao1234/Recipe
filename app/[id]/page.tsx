d57c3448a594f185bd03678dda53e0fed847926d
Olivia Xiao <oxiao1234@gmail.com>
2025-07-24 09:18:46 -0400

    Changing react type?

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,12 +1,14 @@
+import { use } from "react";
 import { RecipeType } from "../types";
 
-type Props = {
-    params: {
-        id: string;
-    }
+type Props = Promise<
+{
+    id: string
 }
+>
 
-export default function RecipePage({ params }: Props){
+export default function RecipePage(props: { params: Props }){
+    const params = use(props.params);
     return (
         <h1>{params.id}</h1>
     );
